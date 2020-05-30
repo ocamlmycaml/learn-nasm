7424a45b11f3268627a7fa8a513d7fc9726adf81
Aditya Dhara <aditya1992@gmail.com>
2020-05-30 18:57:42 -0400

    Lesson 5 complete - IMPORTS!

---
+++ lesson5/helloworld-imports.asm
@@ -0,0 +1,25 @@
+; Hello world program
+;
+; make build dir: mkdir build/
+; compile with: nasm -f elf -o build/helloworld-imports.o helloworld-imports.asm
+; Link with: ld -m elf_i386 build/helloworld-imports.o -o build/helloworld-imports
+; Run with ./build/helloworld-imports
+
+%include        'functions.asm'
+
+SECTION .data
+msg1    db      'Hello, brave new World!', 0Ah
+msg2    db      'This is how we recycle code in NASM.', 0Ah
+
+
+SECTION .text
+global _start
+
+_start:
+    mov     eax,    msg1    ; sprint needs message in eax
+    call    sprint
+
+    mov     eax,    msg2    ; sprint needs message in eax
+    call    sprint
+
+    call    quit
