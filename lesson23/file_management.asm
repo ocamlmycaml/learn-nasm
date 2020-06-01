a038cc25c58ed3728e90486042187ee766ef16cf
Aditya Dhara <aditya1992@gmail.com>
2020-06-01 03:07:47 -0400

    Lesson 23: writing to file

---
+++ lesson23/file_management.asm
@@ -0,0 +1,32 @@
+; Managing files
+;
+; make build dir: mkdir build/
+; compile with: nasm -f elf -o build/file_management.o file_management.asm
+; Link with: ld -m elf_i386 build/file_management.o -o build/file_management
+; Run with ./build/file_management
+
+%include        'functions.asm'
+
+
+SECTION .data
+filename    db      'somefile.txt', 00h
+contents    db      'Hello world!', 00h
+
+
+SECTION .text
+global _start
+
+_start:
+    mov     ecx,    0777    ; set the permissions to read, write, execute
+    mov     ebx,    filename
+    mov     eax,    8       ; sys_creat - create a file
+    int     80h
+
+    mov     edx,    12      ; number of bytes to write
+    mov     ecx,    contents
+    mov     ebx,    eax     ; get the file descriptor from
+    mov     eax,    4       ; sys_write
+    int     80h
+
+    cmp     eax,    0
+    call    quit
